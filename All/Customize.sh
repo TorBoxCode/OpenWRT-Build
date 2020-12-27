97578e51e0058d54ee5c16d59438e1eadd4630fe
MapesxGM <76612074+MapesxGM@users.noreply.github.com>
2020-12-27 14:02:51 +0800

    Update Customize.sh

--- All/Customize.sh
+++ All/Customize.sh
@@ -2,7 +2,7 @@
 sed -i 's/R20.12.12/R20.12.26/g' lede/package/lean/default-settings/files/zzz-default-settings
 sed -i 's/OpenWrt/OpenWRT/g' lede/package/lean/default-settings/files/zzz-default-settings
 sed -i 's/192.168.1.1/192.168.5.1/g' lede/package/base-files/files/bin/config_generate
-sed -i 's/Nps 内网穿透/nps/g'  lede/package/lean/luci-app-nps/po/zh-cn/nps.po
+sed -i 's/Nps 内网穿透/nps/g' lede/package/lean/luci-app-nps/po/zh-cn/nps.po
 cp -f Theme/nlogo.png lede/package/lean/luci-theme-netgear/htdocs/luci-static/netgear/nlogo.png
 cp -f Theme/style.css lede/package/lean/luci-theme-netgear/htdocs/luci-static/netgear/css/style.css
 cp -f All/RK3399_1.8_2.2_GHz.patch lede/target/linux/rockchip/patches-5.4/RK3399_1.8_2.2_GHz.patch

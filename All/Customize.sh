fd0a35bacdf51e3238cca5a92d45a4d1b4ae0c6f
MapesxGM <76612074+MapesxGM@users.noreply.github.com>
2020-12-27 13:54:50 +0800

    Update Customize.sh

--- All/Customize.sh
+++ All/Customize.sh
@@ -6,7 +6,7 @@ cp -f Theme/nlogo.png lede/package/lean/luci-theme-netgear/htdocs/luci-static/ne
 cp -f Theme/style.css lede/package/lean/luci-theme-netgear/htdocs/luci-static/netgear/css/style.css
 cp -f All/Show_big.LITTLE_CPU_Freq lede/package/lean/autocore/files/arm/sbin/cpuinfo
 cp -f All/RK3399_1.8_2.2_GHz.patch lede/target/linux/rockchip/patches-5.4/RK3399_1.8_2.2_GHz.patch
-cp -f All/nps.po lede/package/lean/luci-app-nps/po/zh-cn/nps.po
+sed -i 's/Nps 内网穿透/nps/g'  lede/package/lean/luci-app-nps/po/zh-cn/nps.po
 cp -f All/CONFIG_R4S.ini lede/.config
 git apply All/Rockchip_Driver_R4S.patch
 cd lede/package/lean/

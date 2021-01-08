31f2b0eb954cd783097472ea6b3cb2e11f17c19a
MapesxGM <76612074+MapesxGM@users.noreply.github.com>
2021-01-08 10:28:23 +0800

    Update Customize.sh

--- All/Customize.sh
+++ All/Customize.sh
@@ -6,6 +6,7 @@ sed -i 's/Nps 内网穿透/nps/g' lede/package/lean/luci-app-nps/po/zh-cn/nps.po
 cp -f Theme/nlogo.png lede/package/lean/luci-theme-netgear/htdocs/luci-static/netgear/nlogo.png
 cp -f Theme/style.css lede/package/lean/luci-theme-netgear/htdocs/luci-static/netgear/css/style.css
 cp -f All/Enable_1.8_2.2_GHz.patch lede/target/linux/rockchip/patches-5.4/Enable_1.8_2.2_GHz.patch
+cp -f All/ssr-p_Makefile lede/feeds/helloworld/luci-app-ssr-plus/Makefile
 cp -f All/Show_CPU_big.LITTLE_FREQ lede/package/lean/autocore/files/arm/sbin/cpuinfo
 cp -f All/CONFIG_R4S.ini lede/.config
 git apply All/Rockchip_Driver_R4S.patch

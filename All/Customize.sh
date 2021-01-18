db8b36e405f2c0636f88ea2892e9d8c28343302e
MapesxGM <76612074+MapesxGM@users.noreply.github.com>
2021-01-18 15:46:07 +0800

    Update Customize.sh

--- All/Customize.sh
+++ All/Customize.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-sed -i 's/R20.12.12/R21.1.15/g' lede/package/lean/default-settings/files/zzz-default-settings
+sed -i 's/R20.12.12/R21.1.18/g' lede/package/lean/default-settings/files/zzz-default-settings
 sed -i 's/OpenWrt/OpenWRT/g' lede/package/lean/default-settings/files/zzz-default-settings
-sed -i 's/192.168.1.1/192.168.5.1/g' lede/package/base-files/files/bin/config_generate
-sed -i 's/Nps 内网穿透/nps/g' lede/package/lean/luci-app-nps/po/zh-cn/nps.po
-sed -i 's/0.26.8/0.26.9/g' lede/package/lean/npc/Makefile
+sed -i 's/192.168.1.1/192.168.1.254/g' lede/package/base-files/files/bin/config_generate
 cp -f Theme/nlogo.png lede/package/lean/luci-theme-netgear/htdocs/luci-static/netgear/nlogo.png
 cp -f Theme/style.css lede/package/lean/luci-theme-netgear/htdocs/luci-static/netgear/css/style.css
 cp -f All/Enable_1.8_2.2_GHz.patch lede/target/linux/rockchip/patches-5.4/Enable_1.8_2.2_GHz.patch

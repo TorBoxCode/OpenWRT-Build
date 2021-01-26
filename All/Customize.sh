deddc79b962c6148950410333734e8af27bf7452
MapesxGM <76612074+MapesxGM@users.noreply.github.com>
2021-01-26 16:25:25 +0800

    Update Customize.sh

--- All/Customize.sh
+++ All/Customize.sh
@@ -7,6 +7,6 @@ cp -f Theme/style.css lede/package/lean/luci-theme-netgear/htdocs/luci-static/ne
 cp -f All/Enable_1.8_2.2_GHz.patch lede/target/linux/rockchip/patches-5.4/Enable_1.8_2.2_GHz.patch
 cp -f All/Show_CPU_big.LITTLE_FREQ lede/package/lean/autocore/files/arm/sbin/cpuinfo
 cp -f All/CONFIG_R4S.ini lede/.config
-git apply All/Rockchip_Driver_R4S.patch
+#git apply All/Rockchip_Driver_R4S.patch
 cd lede/package/lean/
 git clone https://github.com/KFERMercer/luci-app-dockerman

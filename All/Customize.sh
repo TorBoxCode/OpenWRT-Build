6068c9e7ff12196dd90517f2ef80ff91ceb8faf9
MapesxGM <76612074+MapesxGM@users.noreply.github.com>
2021-01-30 14:35:37 +0800

    Update Customize.sh

--- All/Customize.sh
+++ All/Customize.sh
@@ -3,12 +3,9 @@ sed -i 's/R21.1.18/R21.1.30/g' lede/package/lean/default-settings/files/zzz-defa
 sed -i 's/OpenWrt/OpenWRT/g' lede/package/lean/default-settings/files/zzz-default-settings
 sed -i 's/192.168.1.1/192.168.1.254/g' lede/package/base-files/files/bin/config_generate
 sed -i 's/kmod-r8169/kmod-r8168/g' lede/target/linux/rockchip/image/armv8.mk
-sed -i 's/Nps Setting/nps/g' lede/package/lean/luci-app-nps/luasrc/model/cbi/nps.lua
-sed -i 's/Nps 内网穿透/nps/g' lede/package/lean/luci-app-nps/po/zh-cn/nps.po
-sed -i 's/0.26.8/0.26.9/g' lede/package/lean/npc/Makefile
 cp -f Theme/nlogo.png lede/package/lean/luci-theme-netgear/htdocs/luci-static/netgear/nlogo.png
 cp -f Theme/style.css lede/package/lean/luci-theme-netgear/htdocs/luci-static/netgear/css/style.css
 cp -f All/Enable_1.8_2.2_GHz.patch lede/target/linux/rockchip/patches-5.4/Enable_1.8_2.2_GHz.patch
 cp -f All/Show_CPU_big.LITTLE_FREQ lede/package/lean/autocore/files/arm/sbin/cpuinfo
 cp -f All/CONFIG_R4S.ini lede/.config
-git clone https://github.com/KFERMercer/luci-app-dockerman lede/package/lean/luci-app-dockerman
+#git clone https://github.com/KFERMercer/luci-app-dockerman lede/package/lean/luci-app-dockerman

7dcf1c4817757f07daec746529cf29bb376b285f
MapesxGM <76612074+MapesxGM@users.noreply.github.com>
2021-02-02 15:12:05 +0800

    Update Customize.sh

--- All/Customize.sh
+++ All/Customize.sh
@@ -7,4 +7,3 @@ cp -f Theme/nlogo.png lede/package/lean/luci-theme-netgear/htdocs/luci-static/ne
 cp -f Theme/style.css lede/package/lean/luci-theme-netgear/htdocs/luci-static/netgear/css/style.css
 cp -f All/Enable_1.8_2.2_GHz.patch lede/target/linux/rockchip/patches-5.4/Enable_1.8_2.2_GHz.patch
 cp -f All/Show_CPU_big.LITTLE_FREQ lede/package/lean/autocore/files/arm/sbin/cpuinfo
-cp -f All/CONFIG_R4S.ini lede/.config

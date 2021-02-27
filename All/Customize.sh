4c710bb86c7d304137c27277151078dd04e4d6a8
MapesxGM <76612074+MapesxGM@users.noreply.github.com>
2021-02-27 14:25:34 +0800

    Update Customize.sh

--- All/Customize.sh
+++ All/Customize.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -i 's/R21.2.1/R21.2.26/g' lede/package/lean/default-settings/files/zzz-default-settings
+sed -i 's/R21.2.1/R21.2.28/g' lede/package/lean/default-settings/files/zzz-default-settings
 sed -i 's/192.168.1.1/10.0.0.1/g' lede/package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/OpenWRT/g' lede/package/lean/default-settings/files/zzz-default-settings
 sed -i 's/Nps 内网穿透/nps/g' lede/package/lean/luci-app-nps/po/zh-cn/nps.po
@@ -7,4 +7,3 @@ cp -f Theme/nlogo.png lede/package/lean/luci-theme-netgear/htdocs/luci-static/ne
 cp -f Theme/style.css lede/package/lean/luci-theme-netgear/htdocs/luci-static/netgear/css/style.css
 cp -f All/Enable_1.8_2.2_GHz.patch lede/target/linux/rockchip/patches-5.4/Enable_1.8_2.2_GHz.patch
 cp -f All/Enable_big.LITTLE_FREQ lede/package/lean/autocore/files/arm/sbin/cpuinfo
-#git clone https://github.com/KFERMercer/luci-app-dockerman lede/package/lean/luci-app-dockerman

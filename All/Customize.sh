bd6efc002ed905822c5d6da06ffe18f34a419b79
MapesxGM <76612074+MapesxGM@users.noreply.github.com>
2021-03-12 18:09:07 +0800

    Update Customize.sh

--- All/Customize.sh
+++ All/Customize.sh
@@ -6,5 +6,5 @@ sed -i 's/Nps 内网穿透/nps/g' lede/package/lean/luci-app-nps/po/zh-cn/nps.po
 cp -f All/CONFIG.ini lede/.config
 cp -f Theme/nlogo.png lede/package/lean/luci-theme-netgear/htdocs/luci-static/netgear/nlogo.png
 cp -f Theme/style.css lede/package/lean/luci-theme-netgear/htdocs/luci-static/netgear/css/style.css
-cp -f All/Enable_1.5_2.0_GHz.patch lede/target/linux/rockchip/patches-5.4/Enable_1.8_2.2_GHz.patch
+cp -f All/Enable_1.5_2.0_GHz.patch lede/target/linux/rockchip/patches-5.4/Enable_1.5_2.0_GHz.patch
 cp -f All/Enable_big.LITTLE_freq.info lede/package/lean/autocore/files/arm/sbin/cpuinfo

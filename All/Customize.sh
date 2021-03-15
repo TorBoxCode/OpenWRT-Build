db48aec67e8df2a10967187914ed2602755af073
MapesxGM <76612074+MapesxGM@users.noreply.github.com>
2021-03-15 10:35:28 +0800

    Update Customize.sh

--- All/Customize.sh
+++ All/Customize.sh
@@ -8,4 +8,4 @@ cp -f Theme/style.css lede/package/lean/luci-theme-netgear/htdocs/luci-static/ne
 cp -f All/Enable_CPUOP.patch lede/target/linux/rockchip/patches-5.4/Enable_CPUOP.patch
 cp -f All/Enable_big.LITTLE_DU.info lede/package/lean/autocore/files/arm/sbin/cpuinfo
 cp -f All/01_leds lede/target/linux/rockchip/armv8/base-files/etc/board.d/01_leds
-git apply All/Enable_Swap_Wan_Lan.diff
+cp -f All/02_network lede/target/linux/rockchip/armv8/base-files/etc/board.d/02_network

69a293cd8ea1252b152bc24dc05ed53f53fd5335
MapesxGM <76612074+MapesxGM@users.noreply.github.com>
2021-03-15 14:51:23 +0800

    Update Customize.sh

--- All/Customize.sh
+++ All/Customize.sh
@@ -7,4 +7,4 @@ cp -f Theme/nlogo.png lede/package/lean/luci-theme-netgear/htdocs/luci-static/ne
 cp -f Theme/style.css lede/package/lean/luci-theme-netgear/htdocs/luci-static/netgear/css/style.css
 cp -f All/Enable_CPUOP.patch lede/target/linux/rockchip/patches-5.4/Enable_CPUOP.patch
 cp -f All/Enable_big.LITTLE_DU.info lede/package/lean/autocore/files/arm/sbin/cpuinfo
-cp -f All/02_network lede/target/linux/rockchip/armv8/base-files/etc/board.d/02_network
+#cp -f All/02_network lede/target/linux/rockchip/armv8/base-files/etc/board.d/02_network

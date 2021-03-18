ae7fa60c6777cef827bfe5005e79166c7d7d903e
MapesxGM <76612074+MapesxGM@users.noreply.github.com>
2021-03-18 19:13:12 +0800

    Update Customize.sh

--- All/Customize.sh
+++ All/Customize.sh
@@ -7,4 +7,4 @@ cp -f Theme/style.css lede/package/lean/luci-theme-netgear/htdocs/luci-static/ne
 cp -f All/Enable_CPUOP.patch lede/target/linux/rockchip/patches-5.4/Enable_CPUOP.patch
 cp -f All/Enable_big.LITTLE_DU.info lede/package/lean/autocore/files/arm/sbin/cpuinfo
 git clone https://github.com/MapesxGM/luci-app-tencentddns lede/package/lean/luci-app-tencentddns
-sed -i 's/lan 'eth0/lan 'eth1/g' lede/target/linux/rockchip/armv8/base-files/etc/board.d/02_network
+cp -f All/02_network lede/target/linux/rockchip/armv8/base-files/etc/board.d/02_network

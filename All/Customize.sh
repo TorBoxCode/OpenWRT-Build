4d133e53da5045f5bc451ec902fc7539681b9805
MapesxGM <76612074+MapesxGM@users.noreply.github.com>
2021-03-18 14:38:24 +0800

    Update Customize.sh

--- All/Customize.sh
+++ All/Customize.sh
@@ -2,7 +2,6 @@
 sed -i 's/R21.2.1/R21.3.16/g' lede/package/lean/default-settings/files/zzz-default-settings
 sed -i 's/192.168.1.1/10.0.0.1/g' lede/package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/OpenWRT/g' lede/package/lean/default-settings/files/zzz-default-settings
-sed -i 's/Nps 内网穿透/nps/g' lede/package/lean/luci-app-nps/po/zh-cn/nps.po
 cp -f Theme/nlogo.png lede/package/lean/luci-theme-netgear/htdocs/luci-static/netgear/nlogo.png
 cp -f Theme/style.css lede/package/lean/luci-theme-netgear/htdocs/luci-static/netgear/css/style.css
 cp -f All/Enable_CPUOP.patch lede/target/linux/rockchip/patches-5.4/Enable_CPUOP.patch

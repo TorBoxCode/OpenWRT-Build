38672d38cc36560e6f2315961bfc3432ffa7a32e
MapesxGM <76612074+MapesxGM@users.noreply.github.com>
2021-03-18 19:01:54 +0800

    Update Customize.sh

--- All/Customize.sh
+++ All/Customize.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -i 's/R21.2.1/R21.3.16/g' lede/package/lean/default-settings/files/zzz-default-settings
+sed -i 's/R21.2.1/R21.3.18/g' lede/package/lean/default-settings/files/zzz-default-settings
 sed -i 's/192.168.1.1/10.0.0.1/g' lede/package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/OpenWRT/g' lede/package/lean/default-settings/files/zzz-default-settings
 cp -f Theme/nlogo.png lede/package/lean/luci-theme-netgear/htdocs/luci-static/netgear/nlogo.png
@@ -7,3 +7,4 @@ cp -f Theme/style.css lede/package/lean/luci-theme-netgear/htdocs/luci-static/ne
 cp -f All/Enable_CPUOP.patch lede/target/linux/rockchip/patches-5.4/Enable_CPUOP.patch
 cp -f All/Enable_big.LITTLE_DU.info lede/package/lean/autocore/files/arm/sbin/cpuinfo
 git clone https://github.com/MapesxGM/luci-app-tencentddns lede/package/lean/luci-app-tencentddns
+sed -i 's/lan 'eth0/lan 'eth1/g' lede/target/linux/rockchip/armv8/base-files/etc/board.d/02_network

3fcee94504aac340b324923b6ad141582eaef262
MapesxGM <76612074+MapesxGM@users.noreply.github.com>
2021-08-15 20:54:31 +0800

    Update Customize.sh

--- All/Customize.sh
+++ All/Customize.sh
@@ -4,7 +4,7 @@ sed -i 's/192.168.1.1/10.0.0.1/g' lede/package/base-files/files/bin/config_gener
 sed -i 's/OpenWrt/OpenWRT/g' lede/package/lean/default-settings/files/zzz-default-settings
 cp -f Theme/nlogo.png lede/package/lean/luci-theme-netgear/htdocs/luci-static/netgear/nlogo.png
 cp -f Theme/style.css lede/package/lean/luci-theme-netgear/htdocs/luci-static/netgear/css/style.css
-cp -f All/Enable_CPUOP.patch lede/target/linux/rockchip/patches-5.4/Enable_CPUOP.patch
-cp -f All/Enable_CPUOP.patch lede/target/linux/rockchip/patches-5.10/Enable_CPUOP.patch
+#cp -f All/Enable_CPUOP.patch lede/target/linux/rockchip/patches-5.4/Enable_CPUOP.patch
+#cp -f All/Enable_CPUOP.patch lede/target/linux/rockchip/patches-5.10/Enable_CPUOP.patch
 cp -f All/Enable_big.LITTLE_DU.info lede/package/lean/autocore/files/arm/sbin/cpuinfo
 sed -i 's/5.4/5.10/g' lede/target/linux/rockchip/Makefile

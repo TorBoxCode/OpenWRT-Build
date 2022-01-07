362f7851e93350597b7d1836e92c0e90886b8744
MapesxGM <76612074+MapesxGM@users.noreply.github.com>
2022-01-07 23:03:11 +0800

    Update Customize.sh

--- All/Customize.sh
+++ All/Customize.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-sed -i 's/RR22.1.1/R22.1.6/g' lede/package/lean/default-settings/files/zzz-default-settings
+sed -i 's/R22.1.1/R22.1.10/g' lede/package/lean/default-settings/files/zzz-default-settings
 sed -i 's/192.168.1.1/10.0.0.1/g' lede/package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/OpenWRT/g' lede/package/lean/default-settings/files/zzz-default-settings
 cp -f Theme/nlogo.png lede/package/lean/luci-theme-netgear/htdocs/luci-static/netgear/nlogo.png
 cp -f Theme/style.css lede/package/lean/luci-theme-netgear/htdocs/luci-static/netgear/css/style.css
-#cp -f All/Enable_CPUOP.patch lede/target/linux/rockchip/patches-5.4/Enable_CPUOP.patch
-#cp -f All/Enable_CPUOP.patch lede/target/linux/rockchip/patches-5.10/Enable_CPUOP.patch
+cp -f All/Enable_CPUOP.patch lede/target/linux/rockchip/patches-5.4/Enable_CPUOP.patch
+rm -rf lede/target/linux/rockchip/patches-5.4/992-rockchip-rk3399-overclock-to-2.2-1.8-GHz-for-NanoPi4.patch
+cp -f All/Enable_CPUOP.patch lede/target/linux/rockchip/patches-5.10/Enable_CPUOP.patch
+rm -rf lede/target/linux/rockchip/patches-5.10/992-rockchip-rk3399-overclock-to-2.2-1.8-GHz-for-NanoPi4.patch
 sed -i 's/5.4/5.10/g' lede/target/linux/rockchip/Makefile

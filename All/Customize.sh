e030a384582b3952ce1233aac75765f30c66c064
TorBoxCode <76612074+TorBoxCode@users.noreply.github.com>
2022-12-14 00:46:24 +0800

    Update Customize.sh

--- All/Customize.sh
+++ All/Customize.sh
@@ -3,6 +3,6 @@ sed -i 's/R22.12.1/R22.12.13/g' lede/package/lean/default-settings/files/zzz-def
 sed -i 's/OpenWrt/OpenWRT/g' lede/package/lean/default-settings/files/zzz-default-settings
 sed -i 's/192.168.1.1/10.0.0.1/g' lede/package/base-files/files/bin/config_generate
 sed -i 's/PADDING=1//g' lede/target/linux/rockchip/image/Makefile
-sed -i 's/5.15/6.0/g' lede/target/linux/rockchip/Makefile
+sed -i 's/5.15/6.1/g' lede/target/linux/rockchip/Makefile
 cp -f Theme/nlogo.png lede/feeds/luci/themes/luci-theme-netgear/htdocs/luci-static/netgear/nlogo.png
 cp -f Theme/style.css lede/feeds/luci/themes/luci-theme-netgear/htdocs/luci-static/netgear/css/style.css

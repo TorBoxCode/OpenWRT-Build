3e7ca82892d8fcf53358fdc066c5cbca08abc028
TorBoxCode <76612074+TorBoxCode@users.noreply.github.com>
2023-05-10 19:28:30 +0800

    Update GetSourceCode.sh

--- All/GetSourceCode.sh
+++ All/GetSourceCode.sh
@@ -2,4 +2,4 @@
 git clone https://github.com/TorBoxCode/luci-app-eqos lede/package/luci-app-eqos
 git clone https://github.com/TorBoxCode/luci-app-clash lede/package/luci-app-clash
 git clone https://github.com/TorBoxCode/luci-app-tencentddns lede/package/luci-app-tencentddns
-sed -i '$a\src-git helloworld https://github.com/fw876/helloworld' lede/feeds.conf.default
+#sed -i '$a\src-git helloworld https://github.com/fw876/helloworld' lede/feeds.conf.default

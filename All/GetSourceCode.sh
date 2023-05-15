86c0e1b7b7173299bdfb66939bf0f6b2fa3aa5b6
TorBoxCode <76612074+TorBoxCode@users.noreply.github.com>
2023-05-15 10:27:41 +0800

    Update GetSourceCode.sh

--- All/GetSourceCode.sh
+++ All/GetSourceCode.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 git clone https://github.com/TorBoxCode/luci-app-eqos lede/package/luci-app-eqos
 git clone https://github.com/TorBoxCode/luci-app-tencentddns lede/package/luci-app-tencentddns
-git clone https://github.com/chandelures/openwrt-clash lede/package/openwrt-clash
-git clone https://github.com/chandelures/luci-app-simple-clash lede/package/luci-app-simple-clash
+git clone https://github.com/vernesong/OpenClash lede/package/OpenClash
 #sed -i '$a\src-git helloworld https://github.com/fw876/helloworld' lede/feeds.conf.default

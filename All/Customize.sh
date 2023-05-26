87820edf9183f2a644f107bf31a7915f157233d7
TorBoxCode <76612074+TorBoxCode@users.noreply.github.com>
2023-05-26 23:00:36 +0800

    Update Customize.sh

--- All/Customize.sh
+++ All/Customize.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -i 's/R23.5.1/R23.5.18/g' lede/package/lean/default-settings/files/zzz-default-settings
+sed -i 's/R23.5.1/R23.5.26/g' lede/package/lean/default-settings/files/zzz-default-settings
 sed -i 's/OpenWrt/OpenWRT/g' lede/package/lean/default-settings/files/zzz-default-settings
 sed -i 's/192.168.1.1/10.0.0.1/g' lede/package/base-files/files/bin/config_generate
 sed -i 's/PADDING=1//g' lede/target/linux/rockchip/image/Makefile

0b5bc5b0e0309d75d4516a9299c53e23f4f5e70b
TorBoxCode <76612074+TorBoxCode@users.noreply.github.com>
2023-05-27 01:01:11 +0800

    Update Customize.sh

--- All/Customize.sh
+++ All/Customize.sh
@@ -4,7 +4,5 @@ sed -i 's/OpenWrt/OpenWRT/g' lede/package/lean/default-settings/files/zzz-defaul
 sed -i 's/192.168.1.1/10.0.0.1/g' lede/package/base-files/files/bin/config_generate
 sed -i 's/PADDING=1//g' lede/target/linux/rockchip/image/Makefile
 sed -i 's/5.15/6.1/g' lede/target/linux/rockchip/Makefile
-#rm -rf lede/package/boot/uboot-rockchip/
-#cp -r All/uboot-rockchip49c46be/ lede/package/boot/uboot-rockchip/
 cp -f Theme/nlogo.png lede/feeds/luci/themes/luci-theme-netgear/htdocs/luci-static/netgear/nlogo.png
 cp -f Theme/style.css lede/feeds/luci/themes/luci-theme-netgear/htdocs/luci-static/netgear/css/style.css
